a0f0760c00ae108a27d0da63c41db08f7f385185
ThanhPhu-Dev <tphu2000@gmail.com>
2021-06-11 11:15:31 +0700

    init

---
+++ src/main/java/com/dinhthanhphu/movieticketadmin/MovieTicketAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dinhthanhphu.movieticketadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketAdminApplication.class, args);
+    }
+
+}
