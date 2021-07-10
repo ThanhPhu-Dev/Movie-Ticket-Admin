b8dcc5c736f6aea782d4a6842775a10ef0912c20
ThanhPhu-Dev <tphu2000@gmail.com>
2021-07-10 16:52:33 +0700

    chinh lại sider

---
+++ src/main/java/com/dinhthanhphu/movieticketadmin/api/MovieApi.java
@@ -0,0 +1,9 @@
+package com.dinhthanhphu.movieticketadmin.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieApi {
+
+
+}
--- src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
+++ src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.dinhthanhphu.movieticketadmin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller(value = "movieAdmin")
 public class HomeController {
 
     @GetMapping("/")
