919762fa8acaa45f89442adea82698eba177b9f7
ThanhPhu-Dev <tphu2000@gmail.com>
2021-08-09 11:02:24 +0700

    test1

--- src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
+++ src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller(value = "movieAdmin")
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
 
-    @GetMapping("/test1")
+    @GetMapping("/")
     public String test1(){
         return "test1";
     }
