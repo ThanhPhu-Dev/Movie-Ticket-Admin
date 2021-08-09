1007c515058ceb04aeb7752fde8a47ed98d30acd
ThanhPhu-Dev <tphu2000@gmail.com>
2021-08-09 10:57:49 +0700

    test

--- src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
+++ src/main/java/com/dinhthanhphu/movieticketadmin/controller/HomeController.java
@@ -11,5 +11,13 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
 
+    @GetMapping("/test2")
+    public String test2(){
+        return "views/test2";
+    }
 }
