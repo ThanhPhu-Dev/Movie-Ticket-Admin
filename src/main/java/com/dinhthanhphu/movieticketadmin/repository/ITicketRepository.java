fee287ba5dc1c90e4eab12b295d42b31580d15fa
ThanhPhu-Dev <tphu2000@gmail.com>
2021-08-17 16:34:40 +0700

    fix repository

--- src/main/java/com/dinhthanhphu/movieticketadmin/repository/ITicketRepository.java
+++ src/main/java/com/dinhthanhphu/movieticketadmin/repository/ITicketRepository.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public interface ITicketRepository extends JpaRepository<TicketEntity, UUID> {
     List<TicketEntity> findByBooking(BookingEntity booking);
 
-    @Query(value = "select * from tickets t join bookings b on t.booking_id = b.id join showtimes s on b.showtime_id = s.id where s.id = 2",
+    @Query(value = "select * from tickets t join bookings b on t.booking_id = b.id join showtimes s on b.showtime_id = s.id where s.id = ?1",
            nativeQuery = true)
     List<TicketEntity> findByShowtimeId(Long showtimeId);
 
